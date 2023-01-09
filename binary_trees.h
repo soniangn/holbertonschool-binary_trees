a3a214ccf8dd958d5b903232d2eb1fe1ea3c7ce5
soniangn <nguyen.sonia@gmail.com>
2023-01-09 03:37:23 -0800

    fixed header static

--- binary_trees.h
+++ binary_trees.h
@@ -45,7 +45,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *tree);
-static size_t _height(const binary_tree_t *tree);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 
 #endif
