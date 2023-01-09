04f4e1c6d2b4c45f5563d67b44c40ca64065866a
soniangn <nguyen.sonia@gmail.com>
2023-01-09 04:07:55 -0800

    fixed function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,12 +14,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL)
-	{
-		return (NULL);
-	}
-
 	node = binary_tree_node(parent, value);
 
 	if (parent->left != NULL)
