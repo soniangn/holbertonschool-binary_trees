2823ec9e21f3ada15037a822fec7b4c2b4a52503
soniangn <nguyen.sonia@gmail.com>
2023-01-09 09:19:36 -0800

    updates header

--- binary_trees.h
+++ binary_trees.h
@@ -21,8 +21,14 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/*Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
+
+/*AVL Tree*/
 typedef struct binary_tree_s avl_t;
+
+/*Max binary heap*/
 typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
