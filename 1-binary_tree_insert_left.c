197395da79b01d119bc1300d827be23fb40c46fe
soniangn <nguyen.sonia@gmail.com>
2023-01-09 03:43:01 -0800

    fixed betty errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,34 +8,34 @@
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  * Return: pointer to the new node or NULL on failure
-*/
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node, *tmp;
+	binary_tree_t *node, *tmp;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-    {
-        free(node);
-        return (NULL);
-    }
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
 
-    node->n = value;
-    node->parent = parent;
+	node->n = value;
+	node->parent = parent;
 
-    if (parent->left == NULL)
-    {
-        parent->left = node;
-        node->left = NULL;
-    }
-    else
-    {
-        tmp = parent->left;
-        parent->left = node;
-        node->left = tmp;
-        tmp->parent = node;
-    }
-    
-    return (node);    
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+		node->left = NULL;
+	}
+	else
+	{
+		tmp = parent->left;
+		parent->left = node;
+		node->left = tmp;
+		tmp->parent = node;
+	}
+
+	return (node);
 }
