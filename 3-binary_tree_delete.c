20f7a6ec1e554b9efa11b9a900c0469fb07e249d
soniangn <nguyen.sonia@gmail.com>
2023-01-09 04:18:27 -0800

    function delete and leaf

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,15 +7,15 @@
  * Description: deletes an entire binary tree
  * @tree: pointer to the root of the tree to delete
  * Return: 0
-*/
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
-    
-    free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
